825155123efe24b99a76bd3538de32cf53ab3be0
Mohit Kothari <54737679+mohit-kothari-git@users.noreply.github.com>
2023-03-04 13:48:50 +0000

    Updated details

--- src/components/footer.js
+++ src/components/footer.js
@@ -77,7 +77,7 @@ const Footer = () => {
     if (process.env.NODE_ENV !== 'production') {
       return;
     }
-    fetch('https://api.github.com/repos/bchiang7/v4')
+    fetch('https://api.github.com/repos/mohit-kothari-git/mohit-kothari.github.io')
       .then(response => response.json())
       .then(json => {
         const { stargazers_count, forks_count } = json;
@@ -107,6 +107,7 @@ const Footer = () => {
       <StyledCredit tabindex="-1">
         <a href="https://github.com/bchiang7/v4">
           <div>Designed &amp; Built by Brittany Chiang</div>
+          <div>Revised by Mohit Kothari</div>
 
           {githubInfo.stars && githubInfo.forks && (
             <div className="github-stats">
