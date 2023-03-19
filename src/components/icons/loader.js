272c9843aa5cd9716b30092c1c7629d96851e4ae
Mohit Kothari <54737679+mohit-kothari-git@users.noreply.github.com>
2023-03-19 09:52:30 +0000

    deg

--- src/components/icons/loader.js
+++ src/components/icons/loader.js
@@ -18,7 +18,7 @@ const IconLoader = () => (
                   L 89, 28 z"
       />
     </g>
-    <text x="30" y="70" fill="currentColor" fontSize="55px" fontFamily="Consolas">
+    <text x="40" y="70" fill="currentColor" fontSize="55px" fontFamily="Consolas">
       M
     </text>
   </svg>
