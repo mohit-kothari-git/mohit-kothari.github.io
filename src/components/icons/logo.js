3c0ebf2096bcbb77f90a911c0973cfaf5262645a
Mohit Kothari <54737679+mohit-kothari-git@users.noreply.github.com>
2023-03-19 09:54:46 +0000

    gtoyt

--- src/components/icons/logo.js
+++ src/components/icons/logo.js
@@ -15,7 +15,7 @@ const IconLogo = () => (
         />
       </g>
     </g>
-    <text x="30" y="65" fill="currentColor" fontSize="50px" fontFamily="Consolas">
+    <text x="28" y="65" fill="currentColor" fontSize="50px" fontFamily="Consolas">
       M
     </text>
   </svg>
